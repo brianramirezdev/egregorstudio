0e8a5612db4bf7fe81995afe22e51852f14a6550
Brian <brianramirezdelgadodev@gmail.com>
2025-12-15 19:00:50 +0000

    corregido texto marquee

--- components/sections/ClientsSection.tsx
+++ components/sections/ClientsSection.tsx
@@ -4,7 +4,7 @@ import Marquee from '@/components/marquee';
 export default function ClientsSection() {
     return (
         <section id="clientes">
-            <Marquee text="TOTI SANZ ✧ CLIENTES ✧ MARCAS ✧ PROYECTOS ✧" border xl itemClassName="py-12 md:py-24" />
+            <Marquee text="BRANDING ✧ WEB ✧ MARKETING DIGITAL ✧ ESTRATEGIA ✧" border xl itemClassName="py-12 md:py-24" />
         </section>
     );
 }
